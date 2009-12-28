5279e068d4f021a4a578a3707c8f0744964093a1
Rob Eisenberg <rob@bluespire.com>
2009-12-28 22:11:40 +0000

    -Added caching of IViewModelDescriptions to the DefaultViewModelDescriptionFactory. -DefaultViewModelDescriptionFactory now looks for an attribute implementing IViewModelDescriptionFactory on that type to provide a custom description of that type.

--- src/Caliburn.PresentationFramework/ViewModels/DefaultViewModelDescriptionFactory.cs
+++ src/Caliburn.PresentationFramework/ViewModels/DefaultViewModelDescriptionFactory.cs
@@ -1,6 +1,8 @@
 namespace Caliburn.PresentationFramework.ViewModels
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using Actions;
     using Core;
     using Filters;
@@ -13,6 +15,7 @@ namespace Caliburn.PresentationFramework.ViewModels
     {
         private readonly IServiceLocator _serviceLocator;
         private readonly IActionLocator _actionLocator;
+        private readonly Dictionary<Type, IViewModelDescription> _cache = new Dictionary<Type, IViewModelDescription>();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DefaultViewModelDescriptionFactory"/> class.
@@ -32,6 +35,31 @@ namespace Caliburn.PresentationFramework.ViewModels
         /// <returns>A view model description.</returns>
         public IViewModelDescription Create(Type targetType)
         {
+            IViewModelDescription description;
+
+            if(!_cache.TryGetValue(targetType, out description))
+            {
+                description = CreateCore(targetType);
+                _cache[targetType] = description;
+            }
+
+            return description;
+        }
+
+        /// <summary>
+        /// Creates the actual description.
+        /// </summary>
+        /// <param name="targetType">Type of the target.</param>
+        /// <returns></returns>
+        protected virtual IViewModelDescription CreateCore(Type targetType) 
+        {
+            var customFactory = targetType.GetCustomAttributes(typeof(IViewModelDescriptionFactory), true)
+                .OfType<IViewModelDescriptionFactory>()
+                .FirstOrDefault();
+
+            if (customFactory != null)
+                return customFactory.Create(targetType);
+
             var description = new DefaultViewModelDescription(targetType);
             var filters = new FilterManager(targetType, description, _serviceLocator);
             var actions = _actionLocator.Locate(new ActionLocationContext(_serviceLocator, targetType, filters));
