00c8cdb98da4a245222c4e03de0d95417975d6df
Marco Amendola <marco.amendola@gmail.com>
2010-08-13 09:02:47 +0000

    Added test for Caliburn.Testability against UI loaded from Xaml

---
+++ src/Tests.Caliburn/Fakes/UI/UIWithMarkupExtensionsBuiltWithXaml.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tests.Caliburn.Fakes.UI
+{
+ 
+	public partial class UIWithMarkupExtensionsBuiltWithXaml : UserControl
+	{
+		public UIWithMarkupExtensionsBuiltWithXaml()
+		{
+			InitializeComponent();
+		}
+	}
+}
