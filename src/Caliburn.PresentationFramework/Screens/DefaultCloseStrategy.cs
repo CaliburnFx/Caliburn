f4c71f485e77dd31aadbae176f3d30bdb847b466
Rob Eisenberg <rob@bluespire.com>
2011-03-21 09:42:09 -0400

    Fixed a memory leak in DefaultCloseStrategy.

--- src/Caliburn.PresentationFramework/Screens/DefaultCloseStrategy.cs
+++ src/Caliburn.PresentationFramework/Screens/DefaultCloseStrategy.cs
@@ -1,70 +1,58 @@
-﻿namespace Caliburn.PresentationFramework.Screens
-{
+﻿namespace Caliburn.PresentationFramework.Screens {
     using System;
     using System.Collections.Generic;
 
     /// <summary>
-    /// Used to gather the results from multiple child elements which may or may not prevent closing.
+    ///   Used to gather the results from multiple child elements which may or may not prevent closing.
     /// </summary>
-    /// <typeparam name="T">The type of child element.</typeparam>
-    public class DefaultCloseStrategy<T> : ICloseStrategy<T>
-    {
+    /// <typeparam name = "T">The type of child element.</typeparam>
+    public class DefaultCloseStrategy<T> : ICloseStrategy<T> {
         List<T> closable;
-        IEnumerator<T> enumerator;
         bool finalResult;
-        Action<bool, IEnumerable<T>> callback;
         readonly bool closeConductedItemsWhenConductorCannotClose;
 
         /// <summary>
-        /// Creates an instance of the class.
+        ///   Creates an instance of the class.
         /// </summary>
-        /// <param name="closeConductedItemsWhenConductorCannotClose">Indicates that even if all conducted items are not closable, those that are should be closed.</param>
+        /// <param name = "closeConductedItemsWhenConductorCannotClose">Indicates that even if all conducted items are not closable, those that are should be closed. The default is true.</param>
         public DefaultCloseStrategy(bool closeConductedItemsWhenConductorCannotClose) {
             this.closeConductedItemsWhenConductorCannotClose = closeConductedItemsWhenConductorCannotClose;
         }
 
         /// <summary>
-        /// Executes the strategy.
+        ///   Executes the strategy.
         /// </summary>
-        /// <param name="toClose">Items that are requesting close.</param>
-        /// <param name="callback">The action to call when all enumeration is complete and the close results are aggregated.
-        /// The bool indicates whether close can occur. The enumerable indicates which children should close if the parent cannot.</param>
-        public void Execute(IEnumerable<T> toClose, Action<bool, IEnumerable<T>> callback)
-        {
-            enumerator = toClose.GetEnumerator();
-            this.callback = callback;
-            closable = new List<T>();
+        /// <param name = "toClose">Items that are requesting close.</param>
+        /// <param name = "callback">The action to call when all enumeration is complete and the close results are aggregated.
+        ///   The bool indicates whether close can occur. The enumerable indicates which children should close if the parent cannot.</param>
+        public void Execute(IEnumerable<T> toClose, Action<bool, IEnumerable<T>> callback) {
             finalResult = true;
+            closable = new List<T>();
 
-            Evaluate(true);
+            Evaluate(true, toClose.GetEnumerator(), callback);
         }
 
-        void Evaluate(bool result)
-        {
+        void Evaluate(bool result, IEnumerator<T> enumerator, Action<bool, IEnumerable<T>> callback) {
             finalResult = finalResult && result;
 
-            if(!enumerator.MoveNext())
-            {
+            if(!enumerator.MoveNext()) {
                 callback(finalResult, closeConductedItemsWhenConductorCannotClose ? closable : new List<T>());
                 closable = null;
             }
-            else
-            {
+            else {
                 var current = enumerator.Current;
                 var guard = current as IGuardClose;
-                if(guard != null)
-                {
-                    guard.CanClose(canClose =>{
+                if(guard != null) {
+                    guard.CanClose(canClose => {
                         if(canClose)
                             closable.Add(current);
 
-                        Evaluate(canClose);
+                        Evaluate(canClose, enumerator, callback);
                     });
                 }
-                else
-                {
+                else {
                     closable.Add(current);
-                    Evaluate(true);
+                    Evaluate(true, enumerator, callback);
                 }
             }
         }
