e154f011056bf437b5b307b05088546e06b276c8
Rob Eisenberg <rob@bluespire.com>
2011-04-02 15:29:41 -0400

    Ensured that SequentialResult always calls dispose on the enumerator.

--- src/Caliburn.PresentationFramework/RoutedMessaging/SequentialResult.cs
+++ src/Caliburn.PresentationFramework/RoutedMessaging/SequentialResult.cs
@@ -1,62 +1,55 @@
-﻿namespace Caliburn.PresentationFramework.RoutedMessaging
-{
+﻿namespace Caliburn.PresentationFramework.RoutedMessaging {
     using System;
     using System.Collections.Generic;
     using Core.InversionOfControl;
     using Core.Logging;
 
     /// <summary>
-    /// An implementation of <see cref="IResult"/> that enables sequential execution of multiple results.
+    ///   An implementation of <see cref = "IResult" /> that enables sequential execution of multiple results.
     /// </summary>
-    public class SequentialResult : IResult
-    {
+    public class SequentialResult : IResult {
         static readonly ILog Log = LogManager.GetLog(typeof(SequentialResult));
 
         readonly IEnumerator<IResult> enumerator;
         ResultExecutionContext context;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SequentialResult"/> class.
+        ///   Initializes a new instance of the <see cref = "SequentialResult" /> class.
         /// </summary>
-        /// <param name="enumerator">The enumerator.</param>
-        public SequentialResult(IEnumerator<IResult> enumerator)
-        {
+        /// <param name = "enumerator">The enumerator.</param>
+        public SequentialResult(IEnumerator<IResult> enumerator) {
             this.enumerator = enumerator;
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SequentialResult" /> class.
+        ///   Initializes a new instance of the <see cref = "SequentialResult" /> class.
         /// </summary>
-        /// <param name="results">The results.</param>
+        /// <param name = "results">The results.</param>
         public SequentialResult(IEnumerable<IResult> results)
-            : this(results.GetEnumerator()){}
+            : this(results.GetEnumerator()) {}
 
         /// <summary>
-        /// Occurs when execution has completed.
+        ///   Occurs when execution has completed.
         /// </summary>
         public event EventHandler<ResultCompletionEventArgs> Completed = delegate { };
 
         /// <summary>
-        /// Executes the result using the specified context.
+        ///   Executes the result using the specified context.
         /// </summary>
-        /// <param name="context">The context.</param>
-        public void Execute(ResultExecutionContext context)
-        {
+        /// <param name = "context">The context.</param>
+        public void Execute(ResultExecutionContext context) {
             this.context = context;
             ChildCompleted(null, new ResultCompletionEventArgs());
         }
 
-        private void ChildCompleted(object sender, ResultCompletionEventArgs args)
-        {
-            if(args.Error != null)
-            {
+        void ChildCompleted(object sender, ResultCompletionEventArgs args) {
+            if(args.Error != null) {
                 Log.Error(args.Error);
                 OnComplete(args.Error, false);
                 return;
             }
 
-            if(args.WasCancelled)
-            {
+            if(args.WasCancelled) {
                 Log.Info("Result enumeration cancelled.");
                 OnComplete(null, true);
                 return;
@@ -64,33 +57,27 @@
 
             var previous = sender as IResult;
 
-            if (previous != null)
+            if(previous != null)
                 previous.Completed -= ChildCompleted;
 
+            var moveNextSucceeded = false;
+            try {
+                moveNextSucceeded = enumerator.MoveNext();
+            }
+            catch(Exception ex) {
+                Log.Error(ex);
+                OnComplete(ex, false);
+                return;
+            }
 
-            bool moveNextSucceeded = false;
-			try
-			{
-				moveNextSucceeded = enumerator.MoveNext();
-			}
-			catch (Exception ex)
-			{
-				Log.Error(ex);
-				OnComplete(ex, false);
-				return;
-			}
-
-            if(moveNextSucceeded)
-            {
-                try
-                {
+            if(moveNextSucceeded) {
+                try {
                     var next = enumerator.Current;
                     IoC.BuildUp(next);
                     next.Completed += ChildCompleted;
                     next.Execute(context);
                 }
-                catch(Exception ex)
-                {
+                catch(Exception ex) {
                     Log.Error(ex);
                     OnComplete(ex, false);
                     return;
@@ -99,10 +86,10 @@
             else OnComplete(null, false);
         }
 
-        private void OnComplete(Exception error, bool wasCancelled) 
-        {
+        void OnComplete(Exception error, bool wasCancelled) {
+            enumerator.Dispose();
             Log.Info("Result enumeration complete.");
-            Completed(this, new ResultCompletionEventArgs{ Error = error, WasCancelled = wasCancelled});
+            Completed(this, new ResultCompletionEventArgs { Error = error, WasCancelled = wasCancelled });
         }
     }
 }
