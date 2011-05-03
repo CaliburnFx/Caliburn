b38d419a9122c1022fcd7bdde88c7a87ce609e9f
Rob Eisenberg <rob@bluespire.com>
2011-05-03 16:14:23 -0400

    Updated the event aggregator so that it is polymorphic like Caliburn.Micro 1.1.

--- src/Caliburn.PresentationFramework/ApplicationModel/DefaultEventAggregator.cs
+++ src/Caliburn.PresentationFramework/ApplicationModel/DefaultEventAggregator.cs
@@ -1,92 +1,103 @@
-﻿namespace Caliburn.PresentationFramework.ApplicationModel
-{
+﻿namespace Caliburn.PresentationFramework.ApplicationModel {
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reflection;
     using Core;
     using Core.Logging;
     using Invocation;
 
     /// <summary>
-    /// Enables loosely-coupled publication of and subscription to events.
+    ///   Enables loosely-coupled publication of and subscription to events.
     /// </summary>
-    public class DefaultEventAggregator : IEventAggregator
-    {
+    public class DefaultEventAggregator : IEventAggregator {
         static readonly ILog Log = LogManager.GetLog(typeof(DefaultEventAggregator));
-        readonly List<WeakReference> subscribers = new List<WeakReference>();
-        readonly IDispatcher dispatcher;
+        readonly List<Handler> handlers = new List<Handler>();
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DefaultEventAggregator"/> class.
+        ///   Subscribes an instance to all events declared through implementations of <see cref = "IHandle{T}" />
         /// </summary>
-        /// <param name="dispatcher">The dispatcher.</param>
-        public DefaultEventAggregator(IDispatcher dispatcher)
-        {
-            this.dispatcher = dispatcher;
-        }
-
-        /// <summary>
-        /// Subscribes an instance to all events declared through implementations of <see cref="IHandle{T}"/>
-        /// </summary>
-        /// <param name="instance">The instance to subscribe for event publication.</param>
-        public void Subscribe(object instance)
-        {
-            lock (subscribers)
-            {
-                if (subscribers.Any(reference => reference.Target == instance))
+        /// <param name = "instance">The instance to subscribe for event publication.</param>
+        public void Subscribe(object instance) {
+            lock(handlers) {
+                if(handlers.Any(x => x.Matches(instance)))
                     return;
 
                 Log.Info("Subscribing {0}.", instance);
-                subscribers.Add(new WeakReference(instance));
+                handlers.Add(new Handler(instance));
             }
         }
 
         /// <summary>
-        /// Unsubscribes the instance from all events.
+        ///   Unsubscribes the instance from all events.
         /// </summary>
-        /// <param name="instance">The instance to unsubscribe.</param>
-        public void Unsubscribe(object instance)
-        {
-            lock (subscribers)
-            {
-                var found = subscribers
-                    .FirstOrDefault(reference => reference.Target == instance);
+        /// <param name = "instance">The instance to unsubscribe.</param>
+        public void Unsubscribe(object instance) {
+            lock(handlers) {
+                var found = handlers.FirstOrDefault(x => x.Matches(instance));
 
-                if (found != null)
-                    subscribers.Remove(found);
+                if(found != null)
+                    handlers.Remove(found);
             }
         }
 
         /// <summary>
-        /// Publishes a message.
+        ///   Publishes a message.
         /// </summary>
-        /// <typeparam name="TMessage">The type of message being published.</typeparam>
-        /// <param name="message">The message instance.</param>
-        public void Publish<TMessage>(TMessage message)
-        {
-            WeakReference[] toNotify;
-            lock (subscribers)
-                toNotify = subscribers.ToArray();
+        /// <param name = "message">The message instance.</param>
+        public void Publish(object message) {
+            Handler[] toNotify;
+            lock(handlers)
+                toNotify = handlers.ToArray();
 
-            dispatcher.ExecuteOnUIThread(() =>{
+            Execute.OnUIThread(() => {
                 Log.Info("Publishing {0}.", message);
-                var dead = new List<WeakReference>();
+                var messageType = message.GetType();
 
-                foreach(var reference in toNotify)
-                {
-                    var target = reference.Target as IHandle<TMessage>;
+                var dead = toNotify.Where(handler => !handler.Handle(messageType, message));
 
-                    if(target != null)
-                        target.Handle(message);
-                    else if(!reference.IsAlive)
-                        dead.Add(reference);
-                }
-                if(dead.Count > 0)
-                {
-                    lock(subscribers)
-                        dead.Apply(x => subscribers.Remove(x));
+                if(dead.Any()) {
+                    lock(handlers)
+                        dead.Apply(x => handlers.Remove(x));
                 }
             });
+        }
+
+        class Handler {
+            readonly WeakReference reference;
+            readonly Dictionary<Type, MethodInfo> supportedHandlers = new Dictionary<Type, MethodInfo>();
+
+            public Handler(object handler) {
+                reference = new WeakReference(handler);
+
+                var interfaces = handler.GetType().GetInterfaces()
+                    .Where(x => typeof(IHandle).IsAssignableFrom(x) && x.IsGenericType);
+
+                foreach(var @interface in interfaces) {
+                    var type = @interface.GetGenericArguments()[0];
+                    var method = @interface.GetMethod("Handle");
+                    supportedHandlers[type] = method;
+                }
+            }
+
+            public bool Matches(object instance) {
+                return reference.Target == instance;
+            }
+
+            public bool Handle(Type messageType, object message) {
+                var target = reference.Target;
+                if(target == null)
+                    return false;
+
+                foreach(var pair in supportedHandlers) {
+                    if(pair.Key.IsAssignableFrom(messageType)) {
+                        pair.Value.Invoke(target, new[] { message });
+                        return true;
+                    }
+                }
+
+                return true;
+            }
         }
     }
 }
--- src/Caliburn.PresentationFramework/ApplicationModel/IEventAggregator.cs
+++ src/Caliburn.PresentationFramework/ApplicationModel/IEventAggregator.cs
@@ -1,27 +1,24 @@
-namespace Caliburn.PresentationFramework.ApplicationModel
-{
+namespace Caliburn.PresentationFramework.ApplicationModel {
     /// <summary>
-    /// Enables loosely-coupled publication of and subscription to events.
+    ///   Enables loosely-coupled publication of and subscription to events.
     /// </summary>
-    public interface IEventAggregator
-    {
+    public interface IEventAggregator {
         /// <summary>
-        /// Subscribes an instance to all events declared through implementations of <see cref="IHandle{T}"/>
+        ///   Subscribes an instance to all events declared through implementations of <see cref = "IHandle{T}" />
         /// </summary>
-        /// <param name="instance">The instance to subscribe for event publication.</param>
+        /// <param name = "instance">The instance to subscribe for event publication.</param>
         void Subscribe(object instance);
 
         /// <summary>
-        /// Unsubscribes the instance from all events.
+        ///   Unsubscribes the instance from all events.
         /// </summary>
-        /// <param name="instance">The instance to unsubscribe.</param>
+        /// <param name = "instance">The instance to unsubscribe.</param>
         void Unsubscribe(object instance);
 
         /// <summary>
-        /// Publishes a message.
+        ///   Publishes a message.
         /// </summary>
-        /// <typeparam name="T">The type of message being published.</typeparam>
-        /// <param name="message">The message instance.</param>
-        void Publish<T>(T message);
+        /// <param name = "message">The message instance.</param>
+        void Publish(object message);
     }
 }
