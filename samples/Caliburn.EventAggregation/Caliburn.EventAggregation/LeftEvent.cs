8c7ac95cf3d2dbb1259a4fed1fc599abc13e828b
Rob Eisenberg <rob@bluespire.com>
2011-05-30 13:15:17 -0400

    Improved the EventAggregation sample.

--- samples/Caliburn.EventAggregation/Caliburn.EventAggregation/LeftEvent.cs
+++ samples/Caliburn.EventAggregation/Caliburn.EventAggregation/LeftEvent.cs
@@ -1,5 +1,9 @@
 ﻿namespace Caliburn.EventAggregation {
     public class LeftEvent {
         public int Number;
+
+        public override string ToString() {
+            return "Left " + Number;
+        }
     }
 }
--- samples/Caliburn.EventAggregation/Caliburn.EventAggregation/ShellViewModel.cs
+++ samples/Caliburn.EventAggregation/Caliburn.EventAggregation/ShellViewModel.cs
@@ -1,16 +1,30 @@
 ﻿namespace Caliburn.EventAggregation {
     using System.ComponentModel.Composition;
+    using PresentationFramework;
+    using PresentationFramework.ApplicationModel;
 
     [Export(typeof(IShell))]
-    public class ShellViewModel : IShell {
+    public class ShellViewModel : PropertyChangedBase, IShell, IHandle<object> {
+        string lastEvent = "No Events Yet";
 
         [ImportingConstructor]
-        public ShellViewModel(LeftViewModel left, RightViewModel right) {
+        public ShellViewModel(LeftViewModel left, RightViewModel right, IEventAggregator events) {
             Left = left;
             Right = right;
+
+            events.Subscribe(this);
         }
 
         public LeftViewModel Left { get; private set; }
         public RightViewModel Right { get; private set; }
+
+        public string LastEvent {
+            get { return lastEvent; }
+        }
+
+        public void Handle(object message) {
+            lastEvent = "Last Event: " + message;
+            NotifyOfPropertyChange(() => LastEvent);
+        }
     }
 }
