de7bff12156f8c7e73ba46ab9848895aa63cfe3f
Marco Amendola <marco.amendola@gmail.com>
2011-07-06 23:32:06 +0200

    Added bubbling actions to samples/Caliburn.WindowManagement

--- samples/Caliburn.WindowManagement/Caliburn.WindowManagement/DialogViewModel.cs
+++ samples/Caliburn.WindowManagement/Caliburn.WindowManagement/DialogViewModel.cs
@@ -1,5 +1,22 @@
 ﻿namespace Caliburn.WindowManagement {
     using PresentationFramework.Screens;
+	using Caliburn.PresentationFramework;
+	using System.Diagnostics;
+	using System.Linq;
 
-    public class DialogViewModel : Screen {}
+    public class DialogViewModel : Screen {
+
+		public DialogViewModel()
+		{
+			Buttons = new BindableCollection<int>(Enumerable.Range(1, 3));
+		}
+
+	 
+		public void SomeActionWithParameter(int value)
+		{
+			Debug.Print("SomeActionWithParameter called through bubbling with value={0}", value);
+		}
+
+		public BindableCollection<int> Buttons { get; private set; }
+	}
 }
