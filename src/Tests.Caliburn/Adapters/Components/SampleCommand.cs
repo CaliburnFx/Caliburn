d3d010561379852e609294bca0dd36006c97226f
Corey Kaylor <corey@kaylors.net>
2016-05-05 17:29:24 -0700

    First consolidating projects and removing adapters

--- src/Tests.Caliburn/Adapters/Components/SampleCommand.cs
+++ src/Tests.Caliburn/Adapters/Components/SampleCommand.cs
@@ -1,13 +1,9 @@
 ﻿namespace Tests.Caliburn.Adapters.Components
 {
     using System.ComponentModel.Composition;
-    using Microsoft.Practices.Unity;
-    using Ninject;
-    using StructureMap.Attributes;
-
     public class SampleCommand : ISampleCommand
     {
-        [Dependency, Inject, SetterProperty, DummyBehavior, Import]
+        [DummyBehavior, Import]
         public ILogger Logger { get; set; }
 
         public void Execute()
