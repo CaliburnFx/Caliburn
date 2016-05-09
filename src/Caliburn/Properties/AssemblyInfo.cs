c424a24aa5f741b5dc0ffc713d1ac59a2b2d13a4
Corey Kaylor <corey@kaylors.net>
2016-05-09 10:58:39 -0700

    Added missing assembly attributes from consolidation

--- src/Caliburn/Properties/AssemblyInfo.cs
+++ src/Caliburn/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security;
+using System.Windows.Markup;
 
 [assembly: AssemblyTitle("Caliburn Core Library")]
 [assembly: Guid("b4d684b5-33d5-41ac-b1c6-cc26555cadfd")]
@@ -9,6 +10,18 @@ using System.Security;
 #if !SILVERLIGHT
 [assembly: CLSCompliant(true)]
 #endif
+
+[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.PresentationFramework")]
+[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.PresentationFramework.Actions")]
+[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.PresentationFramework.Commands")]
+[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.PresentationFramework.ApplicationModel")]
+[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.PresentationFramework.ViewModels")]
+[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.PresentationFramework.Views")]
+[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.PresentationFramework.RoutedMessaging")]
+[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.PresentationFramework.RoutedMessaging.Triggers")]
+[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.PresentationFramework.Converters")]
+[assembly: XmlnsDefinition("http://www.caliburnproject.org", "Caliburn.ShellFramework.Menus")]
+[assembly: XmlnsPrefix("http://www.caliburnproject.org", "cal")]
 
 #if !NO_PARTIAL_TRUST
 [assembly: AllowPartiallyTrustedCallers]
