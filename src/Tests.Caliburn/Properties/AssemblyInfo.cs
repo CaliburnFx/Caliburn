b863f3bbc238ae0dc771e9484e4bc57ce098ff01
Corey Kaylor <corey@kaylors.net>
2016-05-19 12:37:23 -0700

    Updated for dotnet cli and rc2

--- src/Tests.Caliburn/Properties/AssemblyInfo.cs
+++ src/Tests.Caliburn/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Xunit;
 
 [assembly: AssemblyTitle("Caliburn (Unit Tests)")]
 [assembly: Guid("32c17916-11a8-4dff-8aa4-eb86c0516995")]
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
