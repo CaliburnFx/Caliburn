6bc05a4c4b40a76b7adccf097aaf90e8c89b3fc3
Corey Kaylor <corey@kaylors.net>
2016-08-02 16:41:58 -0700

    Added build script for tests and artifacts

---
+++ build.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+pushd %~dp0
+
+md artifacts
+call "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" src\Caliburn.sln /p:Confiuration=Release
+if %errorlevel% neq 0 exit /b %errorlevel%
+call dotnet test src/Tests.Caliburn
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo Packing Caliburn Nuget
+call dotnet pack src/Caliburn --configuration Release --output artifacts
+
+popd
