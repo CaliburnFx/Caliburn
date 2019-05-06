5cdfb2b9f9a0b802b8b44b1aba18816bf3583639
Joe McBride <joseph.mcbride@extendhealth.com>
2019-05-05 21:17:44 -0700

    Appveyor

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ XUNIT_CONSOLE=$NUGET_FOLDER/xunit.runner.console/$XUNIT_VERSION/tools/net452/xun
 
 dotnet restore src
 msbuild.exe "$PWD/src/Caliburn.sln" //p:Configuration=$TYPE
-if [[ $? != 0 ]]; then exit $?; fi
+if [[ $? != 0 ]]; then exit 1; fi
 
 $XUNIT_CONSOLE src/Tests.Caliburn/bin/$TYPE/net452/Tests.Caliburn.dll
-if [[ $? != 0 ]]; then exit $?; fi
+if [[ $? != 0 ]]; then exit 1; fi
 
 dotnet pack src/Caliburn/Caliburn.csproj --configuration $TYPE --output $PWD/artifacts --no-restore
 dotnet pack src/Caliburn.Testability/Caliburn.Testability.csproj --configuration $TYPE --output $PWD/artifacts --no-restore
