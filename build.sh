caf42e092a5534443ba119d3cdd7e9ddeb0223f1
Corey D. Kaylor <ckaylor@extendhealth.com>
2020-02-20 10:10:08 -0700

    Updated build script for dotnet cli

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 TYPE=Release
-NUGET_FOLDER=$USERPROFILE/.nuget/packages
-XUNIT_VERSION=2.4.1
-XUNIT_CONSOLE=$NUGET_FOLDER/xunit.runner.console/$XUNIT_VERSION/tools/net452/xunit.console.exe
 
 dotnet restore src
-msbuild.exe "$PWD/src/Caliburn.sln" //p:Configuration=$TYPE
+dotnet build src -c $TYPE
 if [[ $? != 0 ]]; then exit 1; fi
 
-$XUNIT_CONSOLE src/Tests.Caliburn/bin/$TYPE/net452/Tests.Caliburn.dll
+dotnet test src -c $TYPE
 if [[ $? != 0 ]]; then exit 1; fi
 
 dotnet pack src/Caliburn/Caliburn.csproj --configuration $TYPE --output $PWD/artifacts --no-restore
